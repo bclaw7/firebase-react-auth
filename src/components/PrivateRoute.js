51e28f5aa5973819a82d5071b66e0ba3797e6bb2
bclaw <bclaw8300@gmail.com>
2021-06-22 23:05:29 -0600

    fixed ...rest syntax error

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Route, Redirect } from "react-router";
 import { useAuth } from "../contexts/AuthContext";
-export default function PrivateRoute({ component: Component }) {
+export default function PrivateRoute({ component: Component, ...rest }) {
 	const { currentUser } = useAuth();
 	return (
-		<Route>
-			
+		<Route
+			{...rest}
 			render=
 			{(props) => {
 				return currentUser ? <Component {...props} /> : <Redirect to="/login" />;
 			}}
+            >
 		</Route>
 	);
 }
